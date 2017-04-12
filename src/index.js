0c40a88ceb491d50cf84585f58cdf3d12f54d4f7
Kevin Ngo <me@ngokevin.com>
2017-04-12 16:05:57 -0700

    webpack

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require('aframe-controller-cursor-component');
+require('aframe-event-set-component');
+require('aframe-layout-component');
+require('aframe-motion-capture-components');
+require('aframe-physics-system');
+require('aframe-teleport-controls');
+
+require('./components/axis-helper');
+require('./components/create-thing-button');
+require('./components/gamestate');
+require('./components/html-exporter');
+require('./components/palette-handler');
+require('./components/primitive-cloner');
+require('./components/primitive-deleter');
+require('./components/primitive-mover');
+require('./components/primitive-placer');
+require('./components/primitive-scaler');
